d72b52a2eb85ef146b654c686b1eb8e2f3542c56
wmlhwl <l7q0dkmw6@anonaddy.me>
2024-05-16 13:41:02 +0200

    logowanie zaimplementowane

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'views')));
 
-app.get('/api', (req, res) => {
-  console.log(req.query.email);
-  console.log(req.query.password);
-  res.redirect("/main");
+const db = new sqlite3.Database("godzinowki.db");
+const saltRounds = 12;
+app.post('/mainpage', (req, res) => {
+    var sql = "SELECT password FROM users WHERE email = ?"
+    var params = [req.body.email]
+    db.get(sql, params, (err, row) => {
+        if(!row) return res.render("index", {errorMessage: "Użytkownik nie istnieje"});
+
+        bcrypt.compare(req.body.password, row.password, function (err, result) {
+            if(result) return res.render("main_page")
+            return res.render("index", {errorMessage: "Złe hasło"});
+        });
+    });
 })
 
-app.use('/main',(req,res,next) => {
-  res.sendFile(path.join(__dirname,'public','main_page.html'));
-});
-
 app.use('/',(req,res,next) => {
-  res.sendFile(path.join(__dirname,'public','index.html'));
+    res.render("index", {errorMessage: ''});
 });
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
