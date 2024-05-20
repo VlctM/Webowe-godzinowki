934fdf468d3f305beeb4d6e6c200cf5a3a2d1fe4
wmlhwl <l7q0dkmw6@anonaddy.me>
2024-05-20 11:55:05 +0200

    strona główna i wylogowywanie

--- index.js
+++ index.js
@@ -14,32 +14,47 @@ app.use(express.static(path.join(__dirname,'views')));
 
 const db = new sqlite3.Database("godzinowki.db");
 app.post('/mainpage', (req, res) => {
-    var sql = "SELECT password FROM users WHERE email = ?";
+    var sql = "SELECT email, first_name, last_name, role_id, dzial_id, password FROM users WHERE email = ?";
     var params = [req.body.email];
     db.get(sql, params, (err, row) => {
         if(!row) return res.render("index", {errorMessage: "Użytkownik nie istnieje"});
 
         bcrypt.compare(req.body.password, row.password, function (err, result) {
             if(result) {
+                const date = new Date();
+                var session_cookie = crypto.randomBytes(16).toString('hex');
+                db.run("UPDATE users SET session_cookie = ? WHERE email = ?", [session_cookie, req.body.email]);
+
                 if(req.body.remember_me) {
-                    const date = new Date();
                     date.setDate(date.getDate() + 1605);
-                    var session_cookie = crypto.randomBytes(16).toString('hex');
                     res.cookie("session_cookie", session_cookie, {expires: date});
-                    db.run("UPDATE users SET session_cookie = ? WHERE email = ?", [session_cookie, req.body.email]);
                 }
-                return res.render("main_page");
+                else res.cookie("session_cookie", session_cookie);
+
+                return res.render("main_page", {
+                    email: row.email,
+                    first_name: row.first_name,
+                    last_name: row.last_name,
+                    role_id: row.role_id,
+                    dzial_id: row.dzial_id
+                });
             }
             return res.render("index", {errorMessage: "Złe hasło"});
         });
     });
-})
+});
 
 app.use(cookieParser());
 app.use('/',(req,res,next) => {
-    db.get("SELECT email, first_name, last_name, role_id, dzial_id  FROM users WHERE session_cookie = ?", req.cookies.session_cookie, (err, row) => {
+    db.get("SELECT email, first_name, last_name, role_id, dzial_id FROM users WHERE session_cookie = ?", req.cookies.session_cookie, (err, row) => {
         if(!row) return res.render("index", {errorMessage: ''});
-        return res.render("main_page");
+        return res.render("main_page", {
+            email: row.email,
+            first_name: row.first_name,
+            last_name: row.last_name,
+            role_id: row.role_id,
+            dzial_id: row.dzial_id
+        });
     });
 });
 
